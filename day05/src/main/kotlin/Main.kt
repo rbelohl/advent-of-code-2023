65c61fbf5b7836ca4b878cfd99a1c03dcb428d5c
Radek Bělohlávek <radek.belohlavek@email.cz>
2023-12-05 11:13:36 +0100

    day 05 part 2

--- day05/src/main/kotlin/Main.kt
+++ day05/src/main/kotlin/Main.kt
@@ -1,9 +1,9 @@
 import java.io.File
 
-fun findMapping(number: Long, map: MutableList<List<Long>>) : Long {
+fun findMapping(number: Long, map: MutableList<List<Long>>, reverse : Boolean) : Long {
     map.forEach {
-        val destination = it[0]
-        val source = it[1]
+        val destination = if (reverse) it[1] else it[0]
+        val source = if (reverse) it[0] else it[1]
         val length = it[2]
 
         if (number in source ..< source + length) {
@@ -11,6 +11,35 @@ fun findMapping(number: Long, map: MutableList<List<Long>>) : Long {
         }
     }
     return number
+}
+
+fun part1(seeds: List<Long>, maps: MutableList<MutableList<List<Long>>>) : Long {
+    val seedsResult = mutableListOf<Long>()
+    seeds.forEach { seed ->
+        var current = seed
+        maps.forEach { map ->
+            current = findMapping(current, map, false)
+        }
+        seedsResult.add(current)
+    }
+    return seedsResult.min()
+}
+
+fun part2(seeds: List<Long>, maps: MutableList<MutableList<List<Long>>>) : Long {
+    val seedRanges = seeds.chunked(2)
+    var i = 0.toLong()
+    while (true) {
+        var current = i
+        maps.reversed().forEach { map ->
+            current = findMapping(current, map, true)
+        }
+        seedRanges.forEach {
+            if (current in it[0] ..< it[0] + it[1]) {
+                return i
+            }
+        }
+        i++
+    }
 }
 
 fun main(args: Array<String>) {
@@ -36,15 +65,9 @@ fun main(args: Array<String>) {
         maps[mapIndex].add(line.split(" ").map { it.toLong() })
     }
 
-    val seedsResult = mutableListOf<Long>()
-    seeds.forEach { seed ->
-        var current = seed
-        maps.forEach { map ->
-            current = findMapping(current, map)
-        }
-        seedsResult.add(current)
-    }
-    val result1 = seedsResult.min()
-
+    val result1 = part1(seeds, maps)
     println(result1)
+
+    val result2 = part2(seeds, maps)
+    println(result2)
 }
